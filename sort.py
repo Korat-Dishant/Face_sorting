97fcbc5e35598cbc0cffbc12e454dfbad596a7d2
Dishant <koratdishant536631@gmail.com>
2023-05-03 11:00:17 +0530

    final

--- sort.py
+++ sort.py
@@ -4,8 +4,8 @@ import os
 import shutil
 import sys
 
-path_data = "data/"
-path_sorted = "sorted/"
+path_data = "./data/"
+path_sorted = "./sorted/"
 new_image_number = 0
 tf = False
 faces = []
@@ -15,20 +15,19 @@ for data_image in os.listdir(path_data):
     rgb_img = cv2.cvtColor(dimg, cv2.COLOR_BGR2RGB)
     dimg_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-    for i in faces:
-        if i == dimg_encoding :
-            tf = True
-    print(tf, "for image = ", data_image)
-    faces.append(dimg_encoding)
-  
-    # if not tf:
-    #     faces.append(dimg_encoding)
-    #     new_image_number = str(len(faces))
-    #     os.mkdir(path_sorted+new_image_number)
-    #     # move image to new folder 
-    #     shutil.copy(path_data+data_image , path_sorted+new_image_number )
+    res = face_recognition.compare_faces(faces , dimg_encoding )  
+    print(res)
+    #  if no mathes found and the image contain new face
+    if not any(res):
+        new_image_number = str(len(faces))
+        faces.append(dimg_encoding)
+        os.mkdir(path_sorted+new_image_number)
+        # move image to new folder 
+        shutil.copy(path_data+data_image , path_sorted+new_image_number )
     
-    # else:
-    #     # move image to folder of index where its located 
-    #     shutil.copy(path_data+data_image , path_sorted+new_image_number )
+    else:
+        # move image to folder of index where its located 
+        new_image_number = str(res.index(True))
+        shutil.copy(path_data+data_image , path_sorted+new_image_number )
+
 
